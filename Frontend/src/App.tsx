f74f1a06990e841352e1cdf93e5ac38140373f87
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-10-05 01:40:08 +0700

    update

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -21,11 +21,14 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+      </p>
+      <p>
+        HELLO,WORLD
       </p>
     </div>
   )

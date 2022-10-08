d3a9684065644c7a1be4d43ef9321828870bbf4c
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-10-08 20:46:24 +0700

    update database

--- Backend/database/client.go
+++ Backend/database/client.go
@@ -1,11 +1,22 @@
 package database
 
 import (
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+var err error
+
+const DNS = "root:@tcp(127.0.0.1:3306)/dbname?charset=utf8&parseTime=True&loc=Local"
+
 func Connect() {
-	dsn := "root:@tcp(127.0.0.1:3306)/dbname?charset=utf8&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(DNS), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Cannot connect to DB")
+	}
+	// DB.AutoMigrate(&User{})
 }

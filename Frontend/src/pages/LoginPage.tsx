417eba01d57cb853812db25b04210f616f47dd0b
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-10-11 02:11:06 +0700

    update navigate

--- Frontend/src/pages/LoginPage.tsx
+++ Frontend/src/pages/LoginPage.tsx
@@ -1,8 +1,16 @@
+import { useNavigate } from "react-router-dom";
 
-const LoginPage = () => (
+export default function LoginPage() {
+  const navigate = useNavigate();
+
+  const pathHome = () => {
+    navigate("/home");
+  };
+
+  return (
     <div className="showcase text-center">
-        <h1>login</h1>
+      <h1>login</h1>
+      <button onClick={pathHome}>go home</button>
     </div>
-)
-
-export default LoginPage
+  );
+}
